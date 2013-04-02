b906fa22409223dfd48324664b79cefba8672e19
iarielduarte <iariel.javauser@gmail.com>
2013-04-02 18:52:27 +0100

    comitt

--- miscript.py
+++ miscript.py
@@ -1,8 +1,6 @@
 def main():
-	print "Este es un programa del sistema metrico"
-	print "Este es un programa del sistema metrico"
-	print "Este es un programa del sistema metrico"
-	print "Este es un programa del sistema metrico"
+	print "Ademas con python se puede hacer muchas cosas no solo script sin sentidos"
+	print "Realmente si es tan bueno porque no tenemos buena documentacion de python"
 	print "Este es un programa del sistema metrico"
 	millas = input("Cuántas millas?: ")
 	pies = input("Y cuántos pies?: ")
